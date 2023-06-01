c12f4df7089939b25ad36c9a0d7966c68f691924
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-01 10:36:04 +0900

    비교 연산자 예시

---
+++ java_Gp/day3/operator/ComparisonEx.java
@@ -0,0 +1,17 @@
+package day3.operator;
+
+public class ComparisonEx {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1 > 2 : " + (1 > 2));
+		System.out.println("1 < 2 : " + (1 < 2));
+		System.out.println("1 >= 2 : " + (1 >= 2));
+		System.out.println("1 <= 2 : " + (1 <= 2));
+		System.out.println("1 == 2 : " + (1 == 2));
+		System.out.println("1 != 2 : " + (1 != 2));
+	
+		
+	}
+
+}
