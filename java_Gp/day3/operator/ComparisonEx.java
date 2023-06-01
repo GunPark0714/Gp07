2a1eba692c11744d22a70da4ceb86b2395da22cb
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-01 10:40:27 +0900

    Update ComparisonEx.java

--- java_Gp/day3/operator/ComparisonEx.java
+++ java_Gp/day3/operator/ComparisonEx.java
@@ -11,7 +11,19 @@ public class ComparisonEx {
 		System.out.println("1 == 2 : " + (1 == 2));
 		System.out.println("1 != 2 : " + (1 != 2));
 	
-		
+		String Str1 = "abc";
+		String Str2 = "abc";
+		String Str3 = new String("abc");
+		System.out.println("Str1 == Str2 : " + (Str1 == Str2));
+		System.out.println("Str1 == Str3 : " + (Str1 == Str3));
+		System.out.println("Str2 == Str3 : " + (Str2 == Str3));
+	    System.out.println("Str1.equals(Str2) : " + Str1.equals(Str2));
+	    System.out.println("Str1.equals(Str3) : " + Str1.equals(Str3));
+	    System.out.println("Str2.equals(Str3) : " + Str2.equals(Str3));
+
+
+	    
+   	
 	}
 
 }
