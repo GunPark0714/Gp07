d8f8f2ac6afe6dc2a8e42ae44a2d493a823182b4
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-02 14:49:36 +0900

    Update EvenNumberEx2.java

--- java_Gp/day4/ifex/EvenNumberEx2.java
+++ java_Gp/day4/ifex/EvenNumberEx2.java
@@ -4,7 +4,11 @@ public class EvenNumberEx2 {
 
 	public static void main(String[] args) {
 		//홀짝 판별 예제를 조건 연산자를 이용하여 작성
-		int num = 4;
+		int num = 3;
+		String result = num % 2 == 0 ? "even" : "odd";
+		System.out.println(num + " is a " + result + "number.");
+		
+		
 		
 	}
 	}
