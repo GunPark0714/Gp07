e1b1cca086a579d42739691e3129852ceee143f4
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-02 14:49:34 +0900

    Update NumberEx.java

--- java_Gp/day4/homework/NumberEx.java
+++ java_Gp/day4/homework/NumberEx.java
@@ -3,7 +3,9 @@ package day4.homework;
 public class NumberEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* 정수 num가 짝수이면 num에 2를 나누고, 정수 nul이 홀수이면 num에 1을 더한 후
+		 * num을 출력하는 코드를 작성하세요.
+		 */
 
 	}
 
