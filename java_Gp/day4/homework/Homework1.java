7bb2b9b84f56c15bf7a46d0ddd6222b44d3cae6e
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-02 14:49:29 +0900

    Update Homework1.java

--- java_Gp/day4/homework/Homework1.java
+++ java_Gp/day4/homework/Homework1.java
@@ -3,7 +3,8 @@ package day4.homework;
 public class Homework1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* 국어 , 영어, 수학 성적을 입력받아 평균을 출력하는 코드를 작성하세요.
+		 */
 
 	}
 
