068a02e2b57469d37e1578f508a208dfa6430420
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-02 10:14:55 +0900

    If Else문 연습1

--- java_Gp/day4/practice/IfAdultEx.java
+++ java_Gp/day4/practice/IfAdultEx.java
@@ -1,5 +1,7 @@
 package day4.practice;
 
+import java.util.Scanner;
+
 public class IfAdultEx {
 
 	public static void main(String[] args) {
@@ -9,10 +11,16 @@ public class IfAdultEx {
 		 */
 		
 		int age = 19;
+		Scanner sc = new Scanner(System.in);
+		System.out.println( "input age : ");
+		age = sc.nextInt();
+		
+		//나이가 20세 이상이면 adult를 출력하고,
+		//아니면 minor로 출력
 		if (age >= 20) {System.out.println( " adult " );}
 		else {System.out.println( " minor ");}
 		
-		
+		sc.close();
 		
 	}
 	
