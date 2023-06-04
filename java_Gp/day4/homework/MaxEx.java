21f80a0b5eff80f72cf2a0dcd8eee18f5609591e
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-04 15:36:42 +0900

    day4. homework 완

--- java_Gp/day4/homework/MaxEx.java
+++ java_Gp/day4/homework/MaxEx.java
@@ -1,12 +1,24 @@
 package day4.homework;
 
+import java.util.Scanner;
 public class MaxEx {
 
 	public static void main(String[] args) {
 		/* 두 정수를 입력받아 큰 수를 출력하는 코드를 작성
 		 * 
 		 */
-	}
+	 int num1, num2; 
+	 Scanner sc = new Scanner(System.in);
+	 System.out.println(" Input num1, num2 ");
+	 num1 = sc.nextInt();
+	 num2 = sc.nextInt();
+	 
+	 if (num1 > num2){System.out.println( num1 );}
+	 else if (num1 == num2) {System.out.println("Imposibble to compare");}
+	 else {System.out.println( num2 );}
+	
+	
+	 sc.close();
+	}}
 	
 
-}
