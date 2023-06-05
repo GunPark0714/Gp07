0b74f283d879c8152dbd6a467cbad3a99d18f7bc
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-05 15:01:01 +0900

    While 문 구구단 예제

---
+++ java_Gp/day5/Practice/MultiplicationTableEx.java
@@ -0,0 +1,15 @@
+package day5.Practice;
+
+public class MultiplicationTableEx {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		while ( i <= 9) {
+		System.out.println( "2 x" + i + "=" + i*2);
+		++i;
+	}
+	}
+	
+}
