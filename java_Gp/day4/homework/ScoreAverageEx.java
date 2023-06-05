bff68b7fce6d29c7c9e0b8e649ff91d8be3817f8
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-05 10:11:45 +0900

    day 4 homework1 풀이

--- java_Gp/day4/homework/ScoreAverageEx.java
+++ java_Gp/day4/homework/ScoreAverageEx.java
@@ -1,6 +1,7 @@
 package day4.homework;
 
 import java.util.Scanner;
+
 public class ScoreAverageEx {
 
 	public static void main(String[] args) {
@@ -8,13 +9,21 @@ public class ScoreAverageEx {
 		 * 
 		 */
 
-	    int num1, num2, num3;
+	    int Kor, Eng, Math;
+	    double avg;
 	    Scanner sc = new Scanner(System.in);
-	    System.out.println("국어, 영어, 수학 성적을 입력하세요");
-	    num1 = sc.nextInt();
-	    num2 = sc.nextInt();
-	    num3 = sc.nextInt();
-	    int avg = ( (num1 + num2 + num3) / 3);
+	    System.out.println("국어, 영어, 수학 순으로 성적을 입력하세요. ex : 100 90 80 : ");
+	    
+	    //국어 , 영어, 수학순으로 성적을 입력
+	    
+	    //평균을 계산
+	    
+	    //평균을 출력
+	    
+	    Kor = sc.nextInt();
+	    Eng = sc.nextInt();
+	    Math = sc.nextInt();
+	    avg = ( (Kor + Eng + Math) / 3.0); //(double)로 형변환도 가능. 3.0을 이용도 가능
 	    
 	    System.out.println( " 평균점수는" + avg + "점 입니다." );
 	    sc.close();
