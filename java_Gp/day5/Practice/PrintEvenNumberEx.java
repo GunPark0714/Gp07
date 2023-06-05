664e709cefe122cdfee46efca7cab2622bff84a3
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-05 15:00:53 +0900

    While문 짝수 출력 예제

---
+++ java_Gp/day5/Practice/PrintEvenNumberEx.java
@@ -0,0 +1,14 @@
+package day5.Practice;
+
+public class PrintEvenNumberEx {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		while (i <= 10) {System.out.println( i%2 == 0 );
+		++i;
+	}
+
+	}
+}
