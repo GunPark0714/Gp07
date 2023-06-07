113617ade47a5c806f80210d2bbbf7910d83a5b6
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-07 12:26:34 +0900

    day6 for문 hellowworld 예시

---
+++ java_Gp/day6/forex/HelloWorldEx.java
@@ -0,0 +1,27 @@
+package day6.forex;
+
+public class HelloWorldEx {
+
+	public static void main(String[] args) {
+		/* for문을 이용하여 Hellow world 를 5번 출력하는 예제
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙성 : Hello world 를 출력
+		 */
+		
+		int i;
+		for(i = 1 ; i <= 5 ; i++) {
+			System.out.println("Hello world");
+		}	
+		i = 1;
+	    while (i <= 5) {
+	    	System.out.println("Hello world");
+	        i++;
+		}
+		
+		
+		
+	}
+		
+	}
+
+
