405333af3886678922c40cf4e82a87927ebdbc3a
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-07 13:50:16 +0900

    day6 homework 1

--- java_Gp/day6/homework/DisjointEx.java
+++ java_Gp/day6/homework/DisjointEx.java
@@ -1,25 +1,22 @@
 package day6.homework;
 
 import java.util.Scanner;
+
 public class DisjointEx {
 
 	public static void main(String[] args) {
-		/*  두 정수를 입력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요.
-		 *  서로소 : 두 정수의 최대 공약수가 1인 두 수의 관계
-		 *  input 2 numbers : 3 4
-		 *  disJoint!(서로소)
-		 *  input 2 numbers : 8 4
-		 *  Not disjoint! (서로소 아님)
+		/*
+		 * 두 정수를 입력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요. 서로소 : 두 정수의 최대 공약수가 1인 두 수의 관계
+		 * input 2 numbers : 3 4 disJoint!(서로소) input 2 numbers : 8 4 Not disjoint! (서로소
+		 * 아님)
 		 */
-		
-		
+
 		int num1, num2;
 		int i = 1;
 		int gcd;
-		
+
 		Scanner sc = new Scanner(System.in);
-	
-		
+
 	}
 
 }
