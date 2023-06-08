1fd8d4b5a7c985bcf3246bba9358cffeb86601dc
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-08 23:31:06 +0900

    Create Day7HomeworkBackup.java

---
+++ java_Gp/day6/homework/Day7HomeworkBackup.java
@@ -0,0 +1,24 @@
+package day6.homework;
+
+public class Day7HomeworkBackup {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			for (int j = 0; j<5-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <(2*1)-1; k++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+		for (int i = 4; 1 > 0; i--) {
+			for(int j = 0; j<5-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <(2*i)-1; k++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+	}
+
+}
