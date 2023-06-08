1d5c9f1051ada67caae3c5053767e8a4e98aca31
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-08 15:14:05 +0900

    Update StarEx4.java

--- java_Gp/day7/practice/StarEx4.java
+++ java_Gp/day7/practice/StarEx4.java
@@ -3,25 +3,24 @@ package day7.practice;
 public class StarEx4 {
 
 	public static void main(String[] args) {
-		/*     *    i = 1 공=4 *=1   *= 0
- 		 *    ** *    i = 2 공=3 *=2  *= 1
-		 *   *** **    i = 3 공=2 *=3   *=2
-		 *  **** ***    i = 4 공=1 *=4   *=3
-		 * ***** ****    i = 5 공=0 *=5   *=4
-		 *                공=? *=i   *=i      *=i-1
-		 */
-		
-		
 
-		for (int i = 1; i<=5; i++) {
-		    
-			
-			
-			for(int j = 1; j<= 5-i; j++) {
+		int lineCount = 4;  
+		int spaceCount = lineCount/2 +1;
+		int starCount = 1;
+		
+		for(int i = 0; i<lineCount; i++) {
+			for(int j = 0; j<spaceCount; j++) {
 				System.out.print(' ');
 			}
-			
+			for(int j=0; j<starCount; j++) {
+				System.out.print('*');
+			}
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			spaceCount-=1;
+			starCount+=2;
 			System.out.println();
+		}
 	}
-
-}}
+}
