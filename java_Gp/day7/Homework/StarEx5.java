f6ba2960d317bc08ec218fb4393f054d81a1b84f
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-08 13:34:57 +0900

    day7 homework1

---
+++ java_Gp/day7/Homework/StarEx5.java
@@ -0,0 +1,17 @@
+package day7.Homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+		/*     *         	 공=6 *=1   *= 0
+ 		 *    ** *    		 공=4 *=3   *= 1
+		 *   *** **    		 공=2 *=5   *=2
+		 *  **** ***   		 공=0 *=7   *=3
+		 *                 	 공=5 *=2   *=4
+		 *                   공=3 *=4   *=i-1
+		 *                   공=1 *=6
+ 		 */
+
+	}
+
+}
