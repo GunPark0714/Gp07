5602952cf6732ae13f5554acde201765bca92813
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-08 13:33:56 +0900

    day7 반복문 모형 예제1

---
+++ java_Gp/day7/practice/StarEx1.java
@@ -0,0 +1,44 @@
+package day7.practice;
+
+public class StarEx1 {
+
+	public static void main(String[] args) {
+		/* *****    i = 1  *=5
+ 		 * *****    i = 2  *=5
+		 * *****    i = 3  *=5
+		 * *****    i = 4  *=5
+		 * *****    i = 5  *=5
+		 */
+		/* 외부 반복문
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 실행문 : *i개를 출력후 엔터
+		 **/
+		//* 5개를 출력
+		/* 내부 반복문
+		 * 반복횟수 : j는 1부터 5까지 1씩 증가
+		 * 규칙성 : *을 출력
+		 */
+		//엔터
+        
+
+	    
+		int lineCount = 4;  
+		int spaceCount = lineCount/2 +1;
+		int starCount = 1;
+		
+		for(int i = 0; i<lineCount; i++) {
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			for(int j=0; j<starCount; j++) {
+				System.out.print('*');
+			}
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			spaceCount-=1;
+			starCount+=2;
+			System.out.println();
+		}
+	}
+}
