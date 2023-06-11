5f9af08f16dea6c4e48651c00438a931e506c812
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-11 23:00:17 +0900

    Update MethodMonthEx.java

--- java_Gp/day8/homework/MethodMonthEx.java
+++ java_Gp/day8/homework/MethodMonthEx.java
@@ -9,16 +9,38 @@ public class MethodMonthEx {
 		 * 단 메서드 이용
 		 */
 		
-		int month;
+		int num;
 		Scanner sc = new Scanner(System.in);
-		System.out.println();
-		month = sc.nextInt();
+		System.out.println("input num : ");
+	    num = sc.nextInt();
+		String month = getSeason(num);
 		
-		public static String getSeason(int month) {
-			switch(month) {case 1 : return "January" ;}
-			switch(month) {case 1 : return "January" ;}
-			switch(month) {case 1 : return "January" ;}
+		
+		System.out.println( num + " is " +  getSeason(num));
+		
+		sc.close();
+		}
+		
+		public static String getSeason(int num) {
+		    if(num == 1) {return "January" ;}
+		    else if(num == 2) {return "Fabuary" ;}
+		    else if(num == 3) {return "March" ;}
+		    else if(num == 4) {return "April" ;}
+		    else if(num == 5) {return "May" ;}
+		    else if(num == 6) {return "June" ;}
+		    else if(num == 7) {return "July" ;}
+		    else if(num == 8) {return "August" ;}
+		    else if(num == 9) {return "September" ;}
+		    else if(num == 10) {return "October" ;}
+		    else if(num == 11) {return "Novermber" ;}
+		    else if(num == 12) {return "December" ;}
+		    else {return "Wrong month.";}
+		    
+		 }
+			
 	}
 
+
+			
 	
-}
+			
