5f6cfe1d6067e86b7f75b01c8a93a5af71d027ac
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-12 15:11:48 +0900

    Update CarMain.java

--- java_Gp/day9/classEx/CarMain.java
+++ java_Gp/day9/classEx/CarMain.java
@@ -3,8 +3,25 @@ package day9.classEx;
 public class CarMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	     /* 현대 뉴 아반떼 2023년식 자동차 객체를 생성하는 코드를 작성하세요.
+	      * 	
+	      */
+	
+		Car myCar = new Car("The new AVANTE","Hyundai",2023);
+		//현재 내 차 정보를 조회
+		myCar.turn();
+		myCar.setGear('D');
+		myCar.printInfo();
+		myCar.speedChagne(false);
+		myCar.speedChagne(false);
+		myCar.speedChagne(false);
+		myCar.printInfo();
+		myCar.speedChagne(true);
+		myCar.speedChagne(true);
+		myCar.speedChagne(true);
+		myCar.printInfo();
+		myCar.speedChagne(false);
+		myCar.printInfo();
+		
 	}
-
 }
