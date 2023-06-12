b8c94c8f3154bbfab4667d0c78e752ece3a40e1c
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-12 14:33:16 +0900

    Create Class정리.java

---
+++ java_Gp/day9/classEx/Class정리.java
@@ -0,0 +1,21 @@
+package day9.classEx;
+
+public class Class정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/*클래스는 멤버변수, 메서드, 생성자로 구성.
+	 *멤버변수 : 속성
+	 *메서드 : 기능
+	 *ex) 자동차  -> 클래스 
+	 *    속력을 올림 -> 메서드
+	 *    자동차 색상이 검은색 -> 멤버변수
+   	 *    공장에서 막 나온 자동차 -> 생성자
+	 *    
+	 *    
+	 *    
+	 */
+
+}
