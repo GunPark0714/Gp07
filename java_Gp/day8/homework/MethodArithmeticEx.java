78819086a752e3f7fbdd51a165f6506618a1bf1c
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-12 09:39:25 +0900

    day8 homework 메서드 산술연산자 풀이

--- java_Gp/day8/homework/MethodArithmeticEx.java
+++ java_Gp/day8/homework/MethodArithmeticEx.java
@@ -10,41 +10,87 @@ public class MethodArithmeticEx {
 		 * 0으로 나누는 일은 없다고 가정.
 		 */
 		
+		/** 두 정수와 산술 연산자가 주어지면 산술 연산 결과를 알려주는 메서드
+		 * 매개변수 : 두 정수와 산술 연산자 => int numb1, char operator, int num2
+		 * 리턴타입 : 산술 연산 결과 => double 
+		 * 메서드명 : arithmetic
+		 */
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println( "input 2 nums and arithmetic operator : ");
-		
 		int num1, num2;
-		char ch;
+		char operator;
+		double res;
+		System.out.println(" input (ex : 1 + 2) : ");
 		num1 = sc.nextInt();
+		operator = sc.next().charAt(0);
 		num2 = sc.nextInt();
-		ch = sc.next().charAt(0);
-		int sum = num1 + num2;
-		int sub = num1 - num2;
-		int mul = num1 * num2;
-		int div = num1 / num2;
-		int mod = num1 & num2;
-		int result = getArithmetic(ch);
 		
-		System.out.println( result + "is" + getArithmetic(ch));
+		res = Arithmetic(num1, operator, num2);
+		System.out.println( num1 + "" + operator + "" + num2 + " = " + res);
+		
+		res = Arithmetic2(num1, operator, num2);
+		System.out.println( num1 + "" + operator + "" + num2 + " = " + res);
+		
 		
 		sc.close();
 	}
-	    public static int getArithmetic(char ch) {
-		
-	    	if (ch == '+') {int sum = 0;return sum;}
-	    	else if (ch == '-') {int sub = 0; return sub;}
-	    	else if (ch == '*') {int mul = 0; return mul;}
-	    	else if (ch == '/') {int div = 0; return div;}
-	    	else if (ch == '%') {int mod = 0; return mod;}
-			return ch;
-    }
+	    public static double Arithmetic(int num1, char operator, int num2) {
+		    double res = 0.0;
+		    switch(operator) {
+		    case '+': 
+		    	res = num1 + num2;
+		    	break;
+		    case '-':
+		    	res = num1 - num2;
+		    	break;
+		    case '*':
+		    	res = num1 * num2;
+		        break;
+		    case '/':
+		    	res = num1 / (double)num2;
+		        break;
+		    case '%':
+		    	res = num1 % num2;
+		        break;
+		    default:
+		        //잘못된 연산자인 경우 예외처리를 해야 하는데 아직 안배워서 주석처리
+		        //throw new RuntimeException("Wrong operator!");
+		    }
+	    	return res;
+	    }
+	    public static double Arithmetic2(int num1, char operator, int num2) {
+		    switch(operator) {
+		    case '+': 
+		    	return num1 + num2;
+		    	//break; //위에 return이 있기때문에 break가 실행될 일이 절대 없음. => 에러 발생 
+		    case '-':
+		    	return num1 - num2;
+		    	
+		    case '*':
+		    	return num1 * num2;
+		        
+		    case '/':
+		    	return num1 / (double)num2;
+		        
+		    case '%':
+		    	return num1 % num2;
+		        
+		    default:
+		        //잘못된 연산자인 경우 예외처리를 해야 하는데 아직 안배워서 주석처리
+		        //throw new RuntimeException("Wrong operator!");
+		    	return 0.0;
+		    }
+	    }
+	}    	
+			
+	     
+		    
+    
 	    	
 		
 	
 
 		
-	
-	  
-	}	
+
 	
 
