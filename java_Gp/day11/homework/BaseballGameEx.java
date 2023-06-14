b3369ef18247545f4d28db6f11d3d315fdc95a96
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-14 20:37:40 +0900

    day 11 homework1 야구게임

--- java_Gp/day11/homework/BaseballGameEx.java
+++ java_Gp/day11/homework/BaseballGameEx.java
@@ -1,5 +1,6 @@
 package day11.homework;
 
+import java.util.Arrays;
 import java.util.Scanner;
 public class BaseballGameEx {
 
@@ -11,5 +12,62 @@ public class BaseballGameEx {
 		 * 3O : 일치하는 숫자가 하나도 없음
 		 */
 		
+		int min = 1, max = 9;
+        int[]answer = new int[3];
+                
+      
+		boolean checkanswer[] = new boolean[10];
+		for(int i = 0; i<3;) {
+        	int random = (int)(Math.random() * (max - min + 1) + min);
+        	if(!checkanswer[random]) {
+        		answer[i] = random;
+        		i++;
+        		checkanswer[random] = true;
+        	}
+		}
+		for(int i = 0; i<3;i++)
+			System.out.println(answer[i]);
+					
 		
-	}}
+		
+		int[]guess=new int[3];
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" input 3 numbers");
+		for(int i = 0; i<3; i++) {
+			guess[i] = sc.nextInt();
+			
+		}
+		int str = 0;
+		int ball = 0;
+		int out = 0;
+		
+		
+		while(str == 3) {
+		}
+		for(int i = 0; i<3; i++) {
+			for(int j = 0; j<3; j++) {
+				if(answer[i] == guess[j]) {
+				    if(i != j) {
+				    	out++;
+				    	System.out.println("out!");
+				    }
+				    else if(i == j) {
+						str++;
+						System.out.println(" strike :" + str + "," + "ball : " + ball );
+					}	
+					else ball++;
+				    System.out.println(" strike :" + str + "," + "ball : " + ball );
+					}
+				}
+			}
+		
+		if(str==3) {System.out.println(" Player win!");
+		}
+		
+			else {System.out.println("input 3 numbers");		
+			}
+		}
+	}
+		
+		
+	
