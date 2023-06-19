aa8fcc6790e2c37493688127a8eabba720b27e34
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-19 15:02:33 +0900

    Create run.java

---
+++ java_Gp/day13/run/run.java
@@ -0,0 +1,12 @@
+package day13.run;
+
+import day13.homework.controller.VocabularyNoteController;
+
+public class run {
+	
+	public static void main(String[]args) {
+		VocabularyNoteController controller = new VocabularyNoteController();
+		controller.run();
+
+	}
+}
