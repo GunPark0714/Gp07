9e6b48ac227f1d447d81c57201a12530bc1b2ac9
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-20 14:53:19 +0900

    day15 재고관리 예시

---
+++ java_Gp/day15/practice/shop/controller/ShopManager.java
@@ -0,0 +1,15 @@
+package day15.practice.shop.controller;
+
+public class ShopManager {
+
+		
+		public void run() {
+			System.out.println("프로그램을 시작합니다.");
+			
+			System.out.println("프로그램 가동중입니다");
+			
+			System.out.println("프로그램 종료!!");
+		
+	}
+
+}
