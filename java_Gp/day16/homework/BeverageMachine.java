a4d5b67173be6192198eecfbc6ea402d16a9f045
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-21 13:25:30 +0900

    day16 homework

---
+++ java_Gp/day16/homework/BeverageMachine.java
@@ -0,0 +1,58 @@
+package day16.homework;
+
+import java.util.Scanner;
+public class BeverageMachine {
+	
+		Scanner sc = new Scanner(System.in);
+	
+	
+	
+	
+	
+	
+	public void run() {
+		System.out.println("프로그램 시작!");
+			int menu = -1;
+			int EXIT = 4;
+		do {
+			printMenu();
+		
+			menu = sc.nextInt();
+			runMenu(menu);
+	
+}
+
+	private static void printMenu() {
+		System.out.println("==============");
+		System.out.println("메뉴 : " );
+		System.out.println("1. 금액 투입 ");
+		System.out.println("2. 메뉴 선택 ");
+		System.out.println("3. 제품 입고 ");
+		System.out.println("4. 프로그램 종료");
+		System.out.print("메뉴 선택 : ");
+		
+	}
+	private static void runMenu() {
+		System.out.println("=============");
+		switch(menu) {
+		case 1 : 
+			insertmoney();
+			break;
+		case 2 :
+			selectmenu();
+			break;
+		case 3 :
+			store();
+			break;
+		case 4 :
+			break;
+		default;
+			System.out.println("잘못된 메뉴선택!!");
+}
+
+		
+
+	}
+
+}
+}
---
+++ java_Gp/day16/homework/BeverageMachineRun.java
@@ -0,0 +1,12 @@
+package day16.homework;
+
+
+public class BeverageMachineRun {
+
+	public static void main(String[] args) {
+		BeverageMachine beverageMachine = new BeverageMachine();
+		beverageMachine.run();
+	
+			
+		}
+}
