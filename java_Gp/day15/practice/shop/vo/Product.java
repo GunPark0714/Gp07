f3c8623c2bdf7a62962a47af64bca41f042a8a66
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-21 13:25:41 +0900

    day15 shop

--- java_Gp/day15/practice/shop/vo/Product.java
+++ java_Gp/day15/practice/shop/vo/Product.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class Product {
-	//멤버 변수 : 제품 판매와 관련된 정보
-	private String name;//제품명 
+	//멤버 변수 : 제품 판매와 관련된 제품 정보
+	private String name;//제품명
 	private String modelName;//모델명
-	private int price;//가격	
-	private int amount;//수량(판매수량, 전체 재고량)
+	private int price;//가격
+	private int amount;//수량(판매 수량, 전체 제고량)
 	private String category;//분류
 	
 	//생성자
@@ -20,7 +20,14 @@ public class Product {
 		this.category = category;
 	}
 	
-	
+	public Product(Product product) {
+		this.name = product.name;
+		this.modelName = product.modelName;
+		this.price = product.price;
+		this.amount = product.amount;
+		this.category = product.category;
+	}
+
 	//메서드
 	/**제품 입고 기능 => 현재 수량에 주어진 수량을 누적*/
 	public void store(int amount) {
@@ -28,17 +35,30 @@ public class Product {
 		if(amount < 0) {
 			return;
 		}
-		this.amount += amount;
+		accumulate(amount);
 	}
-	/**제품 출고 기능 => 현재 수량에서 주어진 수량을 감소*/
+	/**제품 출고 기능 => 현주 수량에서 주어진 수량을 감소*/
 	public void release(int amount) {
 		if(amount < 0) {
 			return;
 		}
-		this.amount -= amount;
+		accumulate(-amount);
 	}
 	private void accumulate(int amount) {
 		this.amount += amount;
 	}
+
+	public void print() {
+		System.out.println("제품명 : " + name);
+		System.out.println("모델명 : " + modelName);
+		System.out.println("분류  : " + category);
+		System.out.println("제고  : " + amount);
+		System.out.println("가격  : " + price);
+	}
 }
+
+
+
+
+
 
