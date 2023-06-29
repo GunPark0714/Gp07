e2e8197e70f984fe89249a5ec137237a6d9efb1d
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-29 15:18:32 +0900

    Update LambdaEx1.java

--- java_Gp/day22/lambda/LambdaEx1.java
+++ java_Gp/day22/lambda/LambdaEx1.java
@@ -3,22 +3,24 @@ package day22.lambda;
 public class LambdaEx1 {
 
 	public static void main(String[] args) {
-		//InterfaceA a = new InterfaceA(); 불가능 구현클래스를 만들어야함
-		//InterfaceA a = new InterfaceA() {
-
-			/*@Override
-			    InterFace getMax(int num1, int num2);
+		InterfaceA a = new InterfaceA() {
+			public int getMax(int num1, int num2) {
 				return num1 >= num2 ? num1 : num2;
 			}
 		};
-		System.out.println(a.getMax(10,20));
+		System.out.println(a.getMax(10, 20));
+		
 		InterfaceA b = (num1, num2)->{
 			return num1 >= num2 ? num1 : num2;
 		};
 		System.out.println(b.getMax(10, 20));
+		
+		InterfaceA c = (num1, num2)-> num1 >= num2 ? num1 : num2;
+		System.out.println(c.getMax(10, 20));
 	}
 
 }
+
 interface InterfaceA{
 	int getMax(int num1, int num2);
-}*/
+}
