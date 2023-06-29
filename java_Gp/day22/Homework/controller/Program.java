1820870a8499c5c5de9471d36cd610a2e73438a5
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-06-29 14:18:17 +0900

    day22 homework

---
+++ java_Gp/day22/Homework/controller/Program.java
@@ -0,0 +1,10 @@
+package day22.Homework.controller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_Gp/day22/Homework/controller/StudentManager.java
@@ -0,0 +1,22 @@
+package day22.Homework.controller;
+
+public class StudentManager {
+
+	public static void main(String[] args) {
+		/*고등학생을 관리하는 프로그램을 작성하세요
+		 * 학생 관리(입학, 전학(오는학생, 가는 학생), 졸업, 진학)
+		 * 성적 관리
+		 * 면담
+		 */
+		
+		/* 1. 기능을 구체적으로 정리(기능=요구사항 을 어떻게 정리하느냐에 따라 구현이 달라짐)중요*
+		 * 2. 패키지 클래스를 추가(vo 패키지에 추가)
+		 * 3. 기능을 하나씩 구현
+		 */
+		
+		
+		
+		
+	}
+
+}
