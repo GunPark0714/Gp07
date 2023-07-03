0204b342ff5429e3f04eec90e51d0bf667bfc075
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-03 15:05:28 +0900

    day24 네트워크 구현 예시

---
+++ java_Gp/day24/Network/Client.java
@@ -0,0 +1,10 @@
+package day24.Network;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_Gp/day24/Network/ServerEx1.java
@@ -0,0 +1,9 @@
+package day24.Network;
+
+public class ServerEx1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
