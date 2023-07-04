1bf02084ec1cdee022b27d6c677176639e9bb9c6
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-04 15:18:07 +0900

    Create Run.java

---
+++ java_Gp/day23/homework/phone/run/Run.java
@@ -0,0 +1,17 @@
+package day23.homework.phone.run;
+
+import day23.homework.phone.controller.PhoneManager;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		PhoneManager pm = new PhoneManager();
+		pm.run();
+		
+	}
+
+}
+
+
+
