2416a7d71c24bad011618bb61573326ea60c1de8
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-06 17:45:11 +0900

    Update LoanBrowsing.java

--- java_Gp/day26/library/teacher/vo/LoanBrowsing.java
+++ java_Gp/day26/library/teacher/vo/LoanBrowsing.java
@@ -3,6 +3,7 @@ package day26.library.teacher.vo;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 import lombok.Data;
 
@@ -15,18 +16,63 @@ public class LoanBrowsing implements Serializable {
 	private Book book;//대출 도서
 	private Date loanDate;//대출일
 	private Date returnDate;//반납일
+	private Date estimatedDate;
 	
 	public LoanBrowsing(Book book, Date date, int loanPeriod) {
 		this.book = book;
 		this.loanDate = date;
+		caculateReturnDadate(loanPeriod);
 	}
 
 	
 	
+	private void caculateReturnDadate(int loanPeriod) {
+		if(loanDate == null) {
+			return;
+	}
+		Long loanDateMs = loanDate.getTime();
+		Long periodMs =loanPeriod * 24 * 60 * 60 * 1000L;
+		estimatedDate = new Date(loanDateMs + periodMs);
+
 	public String getLoanDateStr() {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy년 MM월 dd일");
 		return format.format(loanDate);
 	}
+
+	public String getReturnDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy년 MM월 dd일");
+		return format.format(returnDate);
+	}
+	Long loanDateMs = loanDate.getTime();
+	Long periodMs = loanPeriod * 24 * 60 * 60 * 1000L;
+	
+	estimatedDate = new Date(loanDateMs + periodMS);
+	
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LoanBrowsing other = (LoanBrowsing) obj;
+		return Objects.equals(book, other.book);
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(book);
+	}
+
+
+
+
+	
 	
 }
 
