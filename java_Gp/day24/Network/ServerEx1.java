79da76feceb54ed97c38a50296578ca287499096
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:18:57 +0900

    Update ServerEx1.java

--- java_Gp/day24/Network/ServerEx1.java
+++ java_Gp/day24/Network/ServerEx1.java
@@ -1,9 +1,27 @@
 package day24.Network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerEx1 {
 
 	public static void main(String[] args) {
-		
+					int port = 5001;
+					ServerSocket Serversocket = null;
+					try {
+						Serversocket = new ServerSocket(port);
+						while(true) {
+							System.out.println("Waiting");
+							Socket socket = Serversocket.accept();
+							System.out.println("connecting");
+						}
+					} catch (IOException e) {
+						System.out.println("Exception");
+						}
+	
+	
+	
+	
 	}
-
 }
