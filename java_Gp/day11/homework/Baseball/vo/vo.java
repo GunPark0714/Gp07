cd39730ae0ba94d3d78192e528155133e420406b
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:19:18 +0900

    Create vo.java

---
+++ java_Gp/day11/homework/Baseball/vo/vo.java
@@ -0,0 +1,26 @@
+package day11.homework.Baseball.vo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import day27.attendanceList.vo.Attendance;
+
+public class vo {
+
+			
+		
+			private List<player>list = new ArrayList<>();
+			private List<Baseballresult>list2 = new ArrayList<>();
+			public void printAttendance() {
+				//결과 기록이 없으면
+				if(list.size() == 0) {
+				System.out.println("등록된 결과가 없습니다.");
+					return;
+					
+			    
+				}
+			
+				}
+	
+	}
+
