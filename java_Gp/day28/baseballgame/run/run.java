291ea1bad7245138934c9e6409b324b53c4bd4e5
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:17:35 +0900

    day28 야구게임 예제

---
+++ java_Gp/day28/baseballgame/run/run.java
@@ -0,0 +1,14 @@
+package day28.baseballgame.run;
+
+import day28.baseballgame.controller.GameController;
+
+public class run {
+
+	public static void main(String[] args) {
+		GameController gc = new GameController();
+		gc.run();
+		
+		
+	}
+
+}
