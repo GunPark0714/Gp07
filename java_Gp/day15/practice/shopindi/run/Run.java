2b3e18812cc68bec27edc139aaee81d29f0b1803
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:19:09 +0900

    Update Run.java

--- java_Gp/day15/practice/shopindi/run/Run.java
+++ java_Gp/day15/practice/shopindi/run/Run.java
@@ -1,10 +1,10 @@
 package day15.practice.shopindi.run;
 
+import day15.practice.shopindi.controller.ShopManager;
+
+
 public class Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	
+		ShopManager sm = new ShopManager();
+}	
