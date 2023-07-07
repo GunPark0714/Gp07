0b2b2ac3c0043b78fea0d588a7e1834b273b522b
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:19:20 +0900

    Create player.java

---
+++ java_Gp/day11/homework/Baseball/vo/player.java
@@ -0,0 +1,17 @@
+package day11.homework.Baseball.vo;
+
+import lombok.Data;
+
+@Data
+public class player {
+
+				
+		private String name;
+
+		public player(String name) {
+			this.name = name;
+		}
+		
+		
+	}
+
