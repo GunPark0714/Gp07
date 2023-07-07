73812836131a798cc472fd6e0be33931248d0cf2
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-07 17:18:59 +0900

    Update Serverclient.java

--- java_Gp/day24/Network/Serverclient.java
+++ java_Gp/day24/Network/Serverclient.java
@@ -1,10 +1,35 @@
 package day24.Network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Serverclient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
+		int port = 5001;
+		ServerSocket serversocket = null;
+		
+		try {
+			serversocket = new ServerSocket(port);
+			while(true) {
+				System.out.println("Waiting");
+			    Socket socket = serversocket.accept();
+			    System.out.println("connected");
+			}
+		} catch (IOException e) {
+			System.out.println("exception!");
+		}
+		
+		
+		
+		int port = 5001;
+		Scanner sc = new Scanner(System.in);
+		socket.connect(new InetSocketaddress(port));
+		System.out.println("connected!");
+		
 	}
 
 }
