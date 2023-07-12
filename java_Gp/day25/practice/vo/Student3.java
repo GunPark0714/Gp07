6f85404bf1fdc8e942d504e024acf708c3a3062a
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-13 08:53:13 +0900

    Update Student3.java

--- java_Gp/day25/practice/vo/Student3.java
+++ java_Gp/day25/practice/vo/Student3.java
@@ -1,12 +1,15 @@
 package day25.practice.vo;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @AllArgsConstructor
 @Data
-public class Student3 {
+public class Student3 implements Serializable{
 
-		 private String name;
+	private static final long serialVersionUID = -3948131167141278183L;
+		private String name;
 		 private String num;
 		 private String major;
 	
