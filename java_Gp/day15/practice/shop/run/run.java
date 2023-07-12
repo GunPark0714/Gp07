d1865c9828ff52bcb82dc2af11cd399f65c65d16
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-13 08:53:04 +0900

    Update run.java

--- java_Gp/day15/practice/shop/run/run.java
+++ java_Gp/day15/practice/shop/run/run.java
@@ -5,6 +5,14 @@ public class run {
 
 	public static void main(String[] args) {
 		//ShopManager 를 이용해서 제품을 관리하는 프로그램을 실행
+		/*1.제품 등록
+		 *2.고객 등록 
+		 *3.고객 정보 조회 
+		 *4.상품 등록
+		 *5.상품 조회 
+		 *6.종료 
+		 */
+		
 		ShopManager shopManager = new ShopManager();
 		shopManager.run();
 	}
