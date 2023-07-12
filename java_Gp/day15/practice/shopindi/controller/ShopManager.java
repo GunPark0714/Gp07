6a91cd23cd6d23b15dfabafc5a35fa2c6c4009db
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-13 08:52:56 +0900

    Update ShopManager.java

--- java_Gp/day15/practice/shopindi/controller/ShopManager.java
+++ java_Gp/day15/practice/shopindi/controller/ShopManager.java
@@ -9,7 +9,18 @@ import day15.practice.shopindi.vo.Product;
 import day17.practice.vo.Sales;
 
 public class ShopManager implements Program{
-
+	/*1.제품 등록
+	 *2.고객 등록 
+	 *3.고객 정보 조회 
+	 *4.상품 등록
+	 *5.상품 조회 
+	 *6.종료 
+	 */
+	
+	
+	
+	
+	
 	
 	private final static int EXIT = 6;
 	Scanner sc = new Scanner(System.in);
@@ -19,105 +30,81 @@ public class ShopManager implements Program{
 	
 	@Override
 	public void printmenu() {
-		System.out.println("메뉴");
-		System.out.println("1. 제품 판매");
-		System.out.println("2. 제품 입고");
-		System.out.println("3. 제품 조회");
-		System.out.println("4. 매출 조회");
-		System.out.println("5. 고객 등록");
-		System.out.println("6. 프로그램 종료");
-		System.out.print("메뉴 선택 : ");
-		
+		System.out.println("1.상품 등록");
+		System.out.println("2.고객 등록");
+		System.out.println("3.고객 정보 조회");
+		System.out.println("4.매출 조회");
+		System.out.println("5.상품 조회");
+		System.out.println("6.종료");
 	}
-	
 	@Override
 	public void run() {
 		int menu;
-		do {
-			System.out.println("===============");
+		do {System.out.println("=================");
 			printmenu();
 			menu = sc.nextInt();
 			runmenu(menu);
-			System.out.println("===============");
-		}while (menu != 6);
+			System.out.println("==================");
+		
 	}
-
 	@Override
 	public void runmenu(int menu) {
 		switch(menu) {
 		case 1 : 
-				sellProduct();
+				inputproduct();
 				break;
-		case 2 : 
-				inputProduct();
+		case 2 :
+				inputcustomer();
 				break;
 		case 3 :
-				printProduct();
+				printcustomer();
 				break;
-		case 4 : 
-				printsales();
+		case 4 :
+				printSales();
 				break;
 		case 5 :
-				inputCustomer();
+				printproduct();
 				break;
 		case 6 : 
-				break;
-		default : System.out.println("wrong input");
-		}
+			    break;
+		default : System.out.println("Wrong input");
 		
-	}
-	private void inputCustomer() {
-		System.out.println("고객 등록");
-		sc.nextLine();
-		System.out.println("고객명 : ");
-		String name = sc.nextLine();
-		System.out.println("멤버쉽 번호 :");
-		String membershipnum = sc.nextLine();	
-		System.out.println("전화번호 : " );
-		String Phonenumber = sc.nextLine();
-		
-		Customer cus = new Customer(name, membershipnum, Phonenumber);
-		if(!customerList.contains(cus)) {
-		    customerList.add(cus);
-		    System.out.println("고객 등록 성공!");
-		}
-		else System.out.println("고객 등록 실패!");
-	}
-
-	private void printsales() {
-		
-	}
-
-	private void printProduct() {
-		System.err.println("상품 등록");
-		sc.nextLine();
-		System.out.println("상품명 : ");
-		String name = sc.nextLine();
-		System.out.println("제조회사 :");
-		String company = sc.nextLine();	
-		System.out.println("가격 : " );
-		String price = sc.nextLine();
-		System.out.println("종류 : ");
-		String type = sc.nextLine();
-		
-		Product pro = new Product(name, company, price, type);
-		if(!productList.contains(pro)) {
-		    productList.add(pro);
-		    System.out.println("상품 등록 성공!");
-		}
-		else System.out.println("상품 등록 실패!");
 	}
 	
-
-	private void inputProduct() {
+	
+	
+	
+		
+	}
+	private void printproduct() {
+		System.out.println();
+	}
+		
+	private void printSales() {
 		// TODO Auto-generated method stub
 		
 	}
-
-	private void sellProduct() {
+	private void printcustomer() {
 		// TODO Auto-generated method stub
+		
+	}
+	private void inputcustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void inputproduct() {
+		System.out.println("상품 등록");
+		sc.nextLine();
+		System.out.println("상품 명 : ");
+		String name = sc.nextLine();
+		System.out.println("상품코드 : ");
+		String num = sc.nextLine();
+		System.out.println("가격 : ");
+		String price = sc.nextLine();
+		
 		
 	}
 }
+
 
 	
