905c0c3baa04aabe85b1b0ddf5d7da8d129e7278
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-13 08:53:11 +0900

    Update Run.java

--- java_Gp/day25/practice/run/Run.java
+++ java_Gp/day25/practice/run/Run.java
@@ -1,12 +1,12 @@
 package day25.practice.run;
 
-import day25.practice.controller.StudentManager;
+import day25.practice.controller.StudentManager3;
 
 public class Run {
 
 	public static void main(String[] args) {
 			
-			StudentManager sm = new StudentManager();
+			StudentManager3 sm = new StudentManager3();
 			sm.run();
 	}	
 	
