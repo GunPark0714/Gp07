5a5fb52f499393ae83312d16d88ecb25ba2defd3
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-07-13 08:52:58 +0900

    Update Program3.java

--- java_Gp/day25/practice/controller/Program3.java
+++ java_Gp/day25/practice/controller/Program3.java
@@ -5,6 +5,6 @@ public interface Program3 {
 		void printMenu();
 		void run();
 		void runMenu(int menu);
-		void load();
-		void save();
+		void load(String FileName);
+		void save(String FileName);
 }
