8ac31378c85542f86780c7c1c26ef4d31754b135
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-08-11 17:01:41 +0900

    Create 28_PROCEDURE_EXAMPLE SELECT_JSON.sql

---
+++ DB/28_PROCEDURE_EXAMPLE SELECT_JSON.sql
@@ -0,0 +1,47 @@
+-- JSON 형태의 문자열을 추출해서 출력하는 프로시저
+-- 학생 정보를 출력하는 프로시저(프로시저 이용 안해도 SELECT문으로 처리 가능하지만 연습을 위해)
+DROP PROCEDURE IF EXISTS SELECT_JSON;
+
+DELIMITER //
+CREATE PROCEDURE SELECT_JSON(
+IN _JSON TEXT 
+)
+BEGIN
+	DECLARE DONE INT DEFAULT 0; -- CURSOR를 이용하여 반복문을 할 때 종료할지를 결정해주는 변수
+	DECLARE _ID CHAR(10); -- 검색결과에 있는 한행의 있는 아이디을 저장할 변수
+    DECLARE _NAME VARCHAR(20); -- 검색결과에 있는 한행의 있는 이름을 저장할 변수
+	
+    
+    DECLARE CUR CURSOR FOR
+		SELECT * FROM JSON_TABLE(_JSON,"$[*]"
+			COLUMNS(
+				ID CHAR(10) PATH "$.ID",
+                NAME VARCHAR(20) PATH "$.NAME"
+            ) 
+		)AS INFO_TABLE
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET DONE = 1;
+    -- CURSOR 를 염
+    OPEN CUR;
+    STUDENT_LOOP = LOOP
+		-- FETCH : CUR에서 한 행씩 꺼내는 작업을 함
+		FETCH CUR INTO _ID, _NAME;
+        -- FETCH를 이용하여 하나씩 꺼낼 때 더 이상 꺼낼 내용이 없으면 
+        -- DONE이 자동으로 1이 됨(왜냐면 16번째줄에서 설정했기 때문에)
+        IF DONE THEN 
+			LEAVE STUDENT_LOOP;
+		END IF;
+        -- 하고싶은 작업 
+        SELECT _ID, _NAME;
+    END LOOP; 
+    -- CURSOR 를 닫음 
+    CLOSE CUR;
+END
+DELIMITER ;
+
+CALL CREATE_JSON('[{"ID":"ABC123","NAME":"홍길동"},{"ID":"ADMIN","NAME":"고길동"},{"ID":"QWE123","NAME":"임꺽정"}]');
+
+INSERT INTO REVIEW(RE_CONTENT, RE_MO_NUM, RE_ME_ID)
+	SELECT '콘크리트 유토피아 재미있어요.' , MO_NUM, 'abc123'
+    FROM MOVIE
+    WHERE MO_TITLE = '콘크리트 유토피아';
+   
