35909f8a8ee9e72669fefc0f4c3931baaf38bdd1
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-08-31 12:39:27 +0900

    1

--- spring_projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring_projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,11 +1,5 @@
 package kr.kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping(value="/")
+	public String home(Model model) {
+		model.addAttribute("name", "abc");
+		return "/main/home";
+	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping(value="/test1")
+	public String test1(Model model, String id, Integer age) {
+		System.out.println("아이디 : " +id);
+		System.out.println("나이 : " + age);
+		return "test1";
 	}
 	
 }
