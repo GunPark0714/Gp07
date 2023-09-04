1ab02aeb366675d0e45bb2c0a2e790352e44d302
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-09-04 17:35:40 +0900

    study 프로젝트 게시판 작성

--- spring_projects/spring/src/main/java/kr/kh/spring/controller/BoardController.java
+++ spring_projects/spring/src/main/java/kr/kh/spring/controller/BoardController.java
@@ -10,18 +10,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
-import kr.kh.spring.paginateion.Criteria;
-import kr.kh.spring.paginateion.PageMaker;
+import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.pagination.PageMaker;
 import kr.kh.spring.service.BoardService;
 import kr.kh.spring.util.Message;
 import kr.kh.spring.vo.BoardVO;
 import kr.kh.spring.vo.MemberVO;
 
-@RequestMapping("/board")
 @Controller
+@RequestMapping("/board")
 public class BoardController {
-	
+
 	@Autowired
 	BoardService boardService;
 	
@@ -38,30 +39,66 @@ public class BoardController {
 		return "/board/list";
 	}
 	
-	
 	@GetMapping("/insert")
 	public String insert() {
 		return "/board/insert";
 	}
 	@PostMapping("/insert")
-	public String insertPost(BoardVO board, HttpSession session, Model model ) {
+	public String insertPost(BoardVO board, HttpSession session, Model model, MultipartFile[] files) {
 		MemberVO user = (MemberVO)session.getAttribute("user");
-		Message msg; 
-		if(boardService.insertBoard(board,user)) {
-			msg = new Message("/board/list","게시글을 등록했습니다.");
+		Message msg;
+		if(boardService.insertBoard(board, user, files)) {
+			msg = new Message("/board/list", "게시글을 등록했습니다.");
 		}else {
-			msg = new Message("/board/insert","게시글을 등록하지 못했습니다.");
+			msg = new Message("/board/insert", "게시글을 등록하지 못했습니다.");
 		}
-		
-		model.addAttribute("msg",msg);
+		model.addAttribute("msg", msg);
 		return "message";
 	}
 	@GetMapping("/detail")
-	public String detail(Model model, Integer bo_num, Criteria cri) {
+	public String detail(Model model, Integer bo_num , Criteria cri) {
 		boardService.updateViews(bo_num);
 		BoardVO board = boardService.getBoard(bo_num);
+		//List<FileVO> fileList = boardService.getFileList(bo_num)
 		model.addAttribute("board", board);
 		model.addAttribute("cri", cri);
 		return "/board/detail";
+	}
+	@GetMapping("/update")
+	public String update(Model model,Integer bo_num, HttpSession session) {
+		BoardVO board = boardService.getBoard(bo_num);
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		if(user == null || board == null || !user.getMe_id().equals(board.getBo_me_id())) {
+			Message msg = new Message("/board/list", "잘못된 접근입니다.");
+			model.addAttribute("msg",msg);
+			return "message";
+		}
+		model.addAttribute("board", board);
+		return "/board/update";
+	}
+	@PostMapping("/update")
+	public String updatePost(Model model, BoardVO board, 
+			MultipartFile[] files, Integer[] delFiles, HttpSession session) {
+		Message msg;
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		if(boardService.updateBoard(board, files, delFiles,user)) {
+			msg = new Message("/board/detail?bo_num="+board.getBo_num(), "게시글을 수정했습니다.");
+		}else {
+			msg = new Message("/board/update?bo_num="+board.getBo_num(), "게시글을 수정하지 못했습니다."); 
+		}
+		model.addAttribute("msg", msg);
+		return "message";
+	}
+	@GetMapping("/delete")
+	public String delete(Model model, HttpSession session, Integer bo_num) {
+		MemberVO user =(MemberVO)session.getAttribute("user");
+		Message msg;
+		if(boardService.deleteBoard(bo_num, user)) {
+			msg = new Message("/board/list", "게시글을 삭제했습니다.");
+		}else {
+			msg = new Message("/board/list", "잘못된 접근입니다.");
+		}
+		model.addAttribute("msg", msg);
+			return "message";
 	}
 }
--- spring_projects/spring/src/main/java/kr/kh/spring/dao/BoardDAO.java
+++ spring_projects/spring/src/main/java/kr/kh/spring/dao/BoardDAO.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import kr.kh.spring.paginateion.Criteria;
+import kr.kh.spring.pagination.Criteria;
 import kr.kh.spring.vo.BoardVO;
+import kr.kh.spring.vo.FileVO;
 
 public interface BoardDAO {
 
@@ -18,5 +19,15 @@ public interface BoardDAO {
 	BoardVO selectBoard(@Param("bo_num")Integer bo_num);
 
 	void updateBoardViews(@Param("bo_num")Integer bo_num);
+
+	void insertFile(@Param("file")FileVO fileVo);
+
+	boolean updateBoard(@Param("board")BoardVO board);
+
+	FileVO selectFile(@Param("fi_num")Integer num);
+
+	void deleteFile(@Param("fi_num")Integer num);
+
+	void deleteBoard(@Param("bo_num")Integer bo_num);
 
 }
