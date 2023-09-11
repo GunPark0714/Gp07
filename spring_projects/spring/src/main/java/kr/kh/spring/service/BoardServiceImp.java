646bd38859f09e24096a8691e3c77e8738a59f96
GunPark0714 <134986541+GunPark0714@users.noreply.github.com>
2023-09-12 00:29:43 +0900

    2

--- spring_projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
+++ spring_projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
@@ -35,12 +35,12 @@ public class BoardServiceImp implements BoardService{
 		if(!boardDao.insertBoard(board)) {
 			return false;
 		}
-		//첨부파일을 업로드
+		//泥⑤��뙆�씪�쓣 �뾽濡쒕뱶
 		if(files == null || files.length == 0) {
 			return true;
 		}
 		
-			//첨부파일을 서버에 업로드 하고, DB에 저장
+			//泥⑤��뙆�씪�쓣 �꽌踰꾩뿉 �뾽濡쒕뱶 �븯怨�, DB�뿉 ���옣
 		uploadFileAndInsert(files, board.getBo_num());
 		
 		return true;
@@ -61,6 +61,63 @@ public class BoardServiceImp implements BoardService{
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	//ap4
+	package com.kh.test.model;
+	 
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.util.ArrayList;
+	import java.util.HashMap;
+	import java.util.List;
+	import java.util.Map;
+	import org.apache.ibatis.session.SqlSession;
+	import org.apache.ibatis.session.SqlSessionFactory;
+	import org.apache.log4j.Logger;
+	 
+	public class TestDao {
+		Logger logger = Logger.getLogger(TestDao.class);
+		public static final String _DRIVER = "oracle.jdbc.driver.OracleDriver";
+		public static final String _URL    = "jdbc:oracle:thin:@192.168.10.3:1521:xe";
+		public static final String _USER   = "kh";
+		public static final String _PW     = "kh";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		private SqlSessionFactory sqlSessionFactory = null;
+		SqlSession sqlSession = null;
+		
+		public TestDao() {
+			sqlSessionFactory = MyBatisCommonFactory.getSqlSessionFactory();
+		}	
+		
+		public List<Test> selectList() {
+			StringBuilder sql = new StringBuilder();
+			sql.append("SELECT seq, writer, title, content, regdate FROM test");
+			List<Test> testList = new ArrayList<>();
+			try {
+				Class.forName(_DRIVER);
+				con = DriverManager.getConnection(_URL, _USER, _PW);
+				pstmt = con.prepareStatement(sql.toString());
+				rs = pstmt.executeQuery();
+				Test test = null;
+				while(rs.next()) {
+					test = new Test();
+					test.setSeq(rs.getInt("seq"));
+					test.setWriter(rs.getString("writer"));
+					test.setTitle(rs.getString("title"));
+					test.setContent(rs.getString("content"));
+					test.setRegdate(rs.getString("regdate"));
+					testList.add(test);
+				}		
+			} catch (Exception e) {
+				logger.info("Exception : "+e.toString());
+			}
+			return testList;
 		}
 	}
 
@@ -101,20 +158,20 @@ public class BoardServiceImp implements BoardService{
 		if(board == null || board.getBo_title()==null || board.getBo_title().length() == 0 ) {
 			return false;
 		}
-		//게시글 정보를 가져옴(로그인한 회원과 작성자가 같은지 확인을 위해) 
+		//寃뚯떆湲� �젙蹂대�� 媛��졇�샂(濡쒓렇�씤�븳 �쉶�썝怨� �옉�꽦�옄媛� 媛숈�吏� �솗�씤�쓣 �쐞�빐) 
 		BoardVO dbBoard = boardDao.selectBoard(board.getBo_num());
-		//db에 해당 게시글이 없거나 게시글 작성자와 로그인한 회원이 다른 경우
+		//db�뿉 �빐�떦 寃뚯떆湲��씠 �뾾嫄곕굹 寃뚯떆湲� �옉�꽦�옄�� 濡쒓렇�씤�븳 �쉶�썝�씠 �떎瑜� 寃쎌슦
 		if(dbBoard == null || !dbBoard.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
 		if(!boardDao.updateBoard(board)) {
 			return false;
 		}
-		//첨부파일 업데이트 
-		//추가된 첨부파일을 서버에 업로드 및 DB에 추가
+		//泥⑤��뙆�씪 �뾽�뜲�씠�듃 
+		//異붽��맂 泥⑤��뙆�씪�쓣 �꽌踰꾩뿉 �뾽濡쒕뱶 諛� DB�뿉 異붽�
 		uploadFileAndInsert(files, board.getBo_num());
 		
-		//삭제된 첨부파일을 서버에서 제거 및 DB에서 제거
+		//�궘�젣�맂 泥⑤��뙆�씪�쓣 �꽌踰꾩뿉�꽌 �젣嫄� 諛� DB�뿉�꽌 �젣嫄�
 		deleteFile(delFiles);
 		return true;
 	}
@@ -128,13 +185,13 @@ public class BoardServiceImp implements BoardService{
 			if(num == null) {
 				continue;
 			}
-			//첨부파일 정보를 가져옴
+			//泥⑤��뙆�씪 �젙蹂대�� 媛��졇�샂
 			FileVO fileVo = boardDao.selectFile(num);
 			if(fileVo == null) {
 				continue;
 			}
 			UploadFileUtils.deleteFile(uploadPath, fileVo.getFi_name());
-			//DB에서 제거 
+			//DB�뿉�꽌 �젣嫄� 
 			boardDao.deleteFile(num);
 		}
 		
@@ -146,14 +203,14 @@ public class BoardServiceImp implements BoardService{
 			return false;
 		}
 		BoardVO board = boardDao.selectBoard(bo_num);
-		//없는 게시글이거나 작성자가 아니면 
+		//�뾾�뒗 寃뚯떆湲��씠嫄곕굹 �옉�꽦�옄媛� �븘�땲硫� 
 		if(board == null || !board.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
-		//첨부파일 삭제
+		//泥⑤��뙆�씪 �궘�젣
 		List<FileVO> fileList = board.getFileVoList();
 		deleteFile(fileList);
-		//게시글 삭제 
+		//寃뚯떆湲� �궘�젣 
 		boardDao.deleteBoard(bo_num);
 		return true;
 	}
@@ -175,26 +232,27 @@ public class BoardServiceImp implements BoardService{
 		if(likeVo == null || likeVo.getLi_me_id() == null) {
 			return -100;
 		}
-		//기존 추천 정보를 가져옴(게시글 번호와 아이디)
+		//湲곗〈 異붿쿇 �젙蹂대�� 媛��졇�샂(寃뚯떆湲� 踰덊샇�� �븘�씠�뵒)
 		LikeVO dbLikeVo = boardDao.selectLike(likeVo.getLi_bo_num(), likeVo.getLi_me_id());
 		
-		//기존 추천 정보가 없으면
+		//湲곗〈 異붿쿇 �젙蹂닿� �뾾�쑝硫�
 		if(dbLikeVo == null) {
-			//추가
+			//異붽�
 			boardDao.insertLike(likeVo);
 		}
-		else {//있으면
-			//db에 있는 추천 상태와 화면에 누른 추천 상태가 같으면 => 취소 
+		else {//�엳�쑝硫�
+			//db�뿉 �엳�뒗 異붿쿇 �긽�깭�� �솕硫댁뿉 �늻瑜� 異붿쿇 �긽�깭媛� 媛숈쑝硫� => 痍⑥냼 
 			if(dbLikeVo.getLi_state() == likeVo.getLi_state()) {
 				likeVo.setLi_state(0);
 			}
-			//업데이트
+			//�뾽�뜲�씠�듃
 			boardDao.updateLike(likeVo);
 		}
 		boardDao.updateBoardLike(likeVo.getLi_bo_num());
 		return likeVo.getLi_state();
 	}
-
+     
+	/se1
 	@Override
 	public LikeVO getBoardLike(Integer bo_num, MemberVO user) {
 		if(bo_num == null || user == null) {
@@ -202,4 +260,147 @@ public class BoardServiceImp implements BoardService{
 		}
 		return boardDao.selectLike(bo_num, user.getMe_id());
 	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException{
+				logger.info("doGet 호출 성공");
+				Board board = new Board();
+				board.setBoardTitle(req.getParameter("btitle"));
+				board.setBoarWriter(req.getParameter("bwriter"));
+				board.setBoardContent(req.getParameter("bcontent"));
+				int result = 0;
+				BoardService bs = new BoardService();
+				result = bs.insertBoard(board);
+				logger.info("result: "+result);
+		 
+				if(result == 1) {
+					res.sendRedirect("/first/blist?page=1");
+				}else {
+					req.setAttribute("message", "게시글 등록 실패!");
+					RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/views/common/error.jsp");
+					view.forward(req, res);
+				}
+			}		
+			
+			public void doPost(HttpServletRequest req, HttpServletResponse res)
+					throws ServletException, IOException{
+				doGet(req,res);
+		}
+			
+	 /se2
+	 public int insertBoard(Connection con, Board board) {
+			int result =0;
+			PreparedStatement pstmt = null;
+			DBConnectionMgr dbMgr = DBConnectionMgr.getInstance();
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO board(BOARD_NUM,BOARD_TITLE,BOARD_WRITER,BOARD_CONTENT)");
+			sql.append("VALUES(seq_khboard.nextval,?,?,?)");
+	 
+			try {
+				// true면 insert후에 자동 커밋
+				con.setAutoCommit(false); // 생략 - 디폴트가 true라서
+				con = dbMgr.getConnection();
+				pstmt = con.prepareStatement(sql.toString());
+				pstmt.setString(1, board.getBoardTitle());
+				pstmt.setString(2, board.getBoardWriter());
+				pstmt.setString(3, board.getBoardContent());
+				result = pstmt.executeUpdate();
+				if(result == 1) {
+					con.commit();
+				}
+			} catch (Exception e) {
+				logger.info("Exception : "+e.toString());
+			} finally {
+				dbMgr.freeConnection(con, pstmt);
+			}
+			return result;
+	}
+	 /se3
+	 
+	 public int insertBoard(Board board) {
+			logger.info("insertBoard 호출 성공");
+			Connection con = getConnection();
+			int result = 0;
+			result = boardDao.insertBoard(con, board);
+	 
+			if(result > 0) {
+				commit(con);
+			} else {
+				rollback(con);
+			}
+			close(con);
+			return result;
+	}
+	 
+	
+	/se4
+	
+	public class JDBCTemplate {
+		 
+		public JDBCTemplate() {
+		}
+	 
+		public static Connection getConnection() {
+			Connection con = null;
+			Properties prop = new Properties();
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				con = DriverManager.getConnection("jdbc:oracle:thin:@128.168.25.30:1521:xe", "student ", "student ");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return con;
+		}
+	 
+		public static void close(Connection con) {
+			try {
+				if (con != null && !con.isClosed()) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	 
+		public static void close(Statement stmt) {
+			try {
+				if (stmt != null && !stmt.isClosed()) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	 
+		public static void close(ResultSet rset) {
+			try {
+				if (rset != null && !rset.isClosed()) {
+					rset.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	 
+		public static void commit(Connection con) {
+			try {
+				if (con != null && !con.isClosed()) {
+					con.commit();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	 
+		public static void rollback(Connection con) {
+			try {
+				if (con != null && !con.isClosed()) {
+					con.rollback();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- spring_projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
+++ spring_projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
@@ -13,6 +13,68 @@ public class BoardVO {
 	private Date bo_reg_date, bo_up_date;
 	
 	private List<FileVO> fileVoList;
+	
+	=ap3
+	package com.kh.test.model;
+	 
+	public class Test {
+		private int    seq 	   = 0;
+		private String writer  = null;
+		private String title   = null;
+		private String content = null;
+		private String regdate = null;
+		
+		public Test() {
+		}
+		
+		public Test(int seq, String writer, String title, String content, String regdate) {
+			this.seq = seq;
+			this.writer = writer;
+			this.title = title;
+			this.content = content;
+			this.regdate = regdate;
+		}
+		
+		public int getSeq() {
+			return seq;
+		}
+		
+		public void setSeq(int seq) {
+			this.seq = seq;
+		}
+		
+		public String getWriter() {
+			return writer;
+		}
+		
+		public void setWriter(String writer) {
+			this.writer = writer;
+		}
+		
+		public String getTitle() {
+			return title;
+		}
+		
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public String getContent() {
+			return content;
+		}
+		
+		public void setContent(String content) {
+			this.content = content;
+		}
+		
+		public String getRegdate() {
+			return regdate;
+		}
+		
+		public void setRegdate(String regdate) {
+			this.regdate = regdate;
+		}
+	}
 
 	public String getBo_up_date_str() {
 		if(bo_up_date == null) {
